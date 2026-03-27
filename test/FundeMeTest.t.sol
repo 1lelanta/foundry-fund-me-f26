3e68328853d4500ca1a4b672805f44caa724d96a
1lelanta <kekikoki4@gmail.com>
2026-03-27 10:10:38 +0300

    Add console import and test for message sender ownership in FundeMeTest

--- test/FundeMeTest.t.sol
+++ test/FundeMeTest.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
 import {FundMe} from "../src/fundeMe.sol";
 
 contract FundeMeTest is Test{
@@ -15,6 +16,10 @@ contract FundeMeTest is Test{
     function testMinimumDollarIsFive() public{
         assertEq(fundme.MINIMUM_USD(),5e18);
 
+    }
+    function testMsgSenderIsOwner() public{
+       
+        assertEq(fundme.i_owner(),address(this));
     }
 
 
