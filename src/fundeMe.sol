915e34ff7bd6a8c7bb4d9aef1b262d37ca342282
1lelanta <kekikoki4@gmail.com>
2026-03-28 09:38:20 +0300

    Fix constructor parameter name and update getVersion function to use class variable

--- src/fundeMe.sol
+++ src/fundeMe.sol
@@ -18,8 +18,10 @@ contract FundMe {
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
     AggregatorV3Interface private s_priceFeed;
 
-    constructor(address piceFeed) {
+
+    constructor(address priceFeed) {
         i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeed)
     }
 
     function fund() public payable {
@@ -30,8 +32,8 @@ contract FundMe {
     }
 
     function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
+       
+        return s_priceFeed.version();
     }
 
     modifier onlyOwner() {
