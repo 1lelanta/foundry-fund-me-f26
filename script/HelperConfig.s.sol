a4d357fbd91449c94d525757fa9c5f3f9e94d337
1lelanta <kekikoki4@gmail.com>
2026-03-28 10:19:41 +0300

    Add activeNetworkConfig and update getSepoliaEthConfig to return NewtorkConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -6,14 +6,19 @@ import{Script} from "forge-std/Script.sol";
 
 contract HelperConfig is Script{
 
+    NewtorkConfig public activeNetworkConfig;
+
     struct NewtorkConfig{
         address priceFeed;
     }
 
 // pure keyword indicates that the function does not read or modify the state of the contract.
 // think of it as a function only performing calculations based on its input paramers always the input gives the same output
-    function getSepoliaEthConfig(NewtorkConfig memory) public pure {
-
+    function getSepoliaEthConfig(NewtorkConfig memory) public pure returns(NewtorkConfig memory){
+        NewtorkConfig memory sepoliaConfig = new NewtorkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
     }
 
     function getAnvilEthConfig(NewtorkConfig memory) public pure{
